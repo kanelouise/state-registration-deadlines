c54f2762ebbc28ff3df819c2033431609c3b0929
kanelouise <kane@kanes-Air.lan>
2025-06-25 16:55:36 -0700

    feat: update tests to vercel url

--- src/app/api/states/api.test.js
+++ src/app/api/states/api.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 
 describe('GET /api/states', () => {
   it('returns a list of states', async () => {
-    const res = await request('http://localhost:3000').get('/api/states')
+    const res = await request('https://state-registration-deadlines.vercel.app/').get('/api/states')
     expect(res.statusCode).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
     expect(res.body[0]).toHaveProperty('State')
